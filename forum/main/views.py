055fe61402fc6ed06aac80df632fb0b162528506
Guriev Nicholas <guriev-ns@ya.ru>
2015-03-08 23:22:21 +0300

    Соощение об ошибке теперь выводится в шаблоне

--- forum/main/views.py
+++ forum/main/views.py
@@ -7,15 +7,16 @@ from main.models import Message
 def forum(request):
     if request.method == 'POST' and 'send' in request.path_info:
         # TODO: выделить этот код в отдельную функцию
-        text = request.POST.get('text', '')
-        if text == '':
-            raise ValueError('You can not send the empty message')
+        text = request.POST.get('text')
+        if not text:
+            return HttpResponseRedirect("/forum?err=empty-msg")
         msg = Message()
         msg.text = text
         if request.user.is_authenticated():
             msg.author = request.user
         msg.save()
-            
         return HttpResponseRedirect("/forum")
-    messages = Message.objects.all()
-    return render(request, 'forum.html', {'messages': messages})
+    context = {'messages': Message.objects.all()}
+    if request.GET.get('err') == 'empty-msg':
+        context['errors'] = 'You can not send the empty message'
+    return render(request, 'forum.html', context)
