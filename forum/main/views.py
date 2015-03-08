b7f368bdb542542f1d19d7800f2facd3da9e2f48
Guriev Nicholas <guriev-ns@ya.ru>
2015-03-08 21:58:16 +0300

    Запретил отправку пустого сообщения

--- forum/main/views.py
+++ forum/main/views.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from django.shortcuts import render
 
 from django.http import HttpResponseRedirect
@@ -5,11 +6,16 @@ from main.models import Message
 
 def forum(request):
     if request.method == 'POST' and 'send' in request.path_info:
+        # TODO: выделить этот код в отдельную функцию
+        text = request.POST.get('text', '')
+        if text == '':
+            raise ValueError('You can not send the empty message')
         msg = Message()
-        msg.text = request.POST.get('text', '')
+        msg.text = text
         if request.user.is_authenticated():
             msg.author = request.user
         msg.save()
+            
         return HttpResponseRedirect("/forum")
     messages = Message.objects.all()
     return render(request, 'forum.html', {'messages': messages})
