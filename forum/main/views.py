39beb4dbd343a31b60a75c13ad8d37e9d7771ce4
Guriev Nicholas <guriev-ns@ya.ru>
2015-03-08 21:03:14 +0300

    Обеспечена защита от двойной отправки сообщений при перезагузке страницы

--- forum/main/views.py
+++ forum/main/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 
+from django.http import HttpResponseRedirect
 from main.models import Message
 
 def forum(request):
-    if request.method == 'POST':
+    if request.method == 'POST' and 'send' in request.path_info:
         text = request.POST.get('text', '')
         msg = Message.objects.create_message(text)
         msg.save()
+        return HttpResponseRedirect("/forum")
     messages = Message.objects.all()
     return render(request, 'forum.html', {'messages': messages})
